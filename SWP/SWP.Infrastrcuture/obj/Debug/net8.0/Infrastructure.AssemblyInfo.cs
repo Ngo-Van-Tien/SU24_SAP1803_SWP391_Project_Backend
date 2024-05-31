f9cf6b8e25c01aff8d377d7182480f25ad0c3f28
quy <quybvse170424@fpt.edu.vn>
2024-05-31 19:45:15 +0700

    31/5

--- SWP/SWP.Infrastrcuture/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ SWP/SWP.Infrastrcuture/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8569b412ad2c43b55b3a3d861b2d21e617586356")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91eb14aed75b0bc4f651658ce154f1c682fbf223")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
